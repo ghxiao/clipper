ee6b3deb67ddb58cc5ef15e1f392c9abc7881d46
Guohui Xiao <gh.xiao@gmail.com>
2012-07-05 22:34:40 +0200

    change default rewriter to CQGraphRewriter, some tests passed

--- clipper-cli/src/test/java/org/semanticweb/clipper/hornshiq/cli/ClipperAppTest.java
+++ clipper-cli/src/test/java/org/semanticweb/clipper/hornshiq/cli/ClipperAppTest.java
@@ -8,7 +8,19 @@ public class ClipperAppTest {
 
 	@Test
 	public void test() {
-		ClipperApp.main("-v=2 rewrite -o /Users/xiao/Dropbox/krrepos/xiao/drafts/idmus/sa/data/BacteriaGeneInteractions.owl".split("\\ ")); 
+		ClipperApp
+				.main("-v=2 rewrite -o /Users/xiao/Dropbox/krrepos/xiao/drafts/idmus/sa/data/BacteriaGeneInteractions.owl"
+						.split("\\ "));
+	}
+
+	@Test
+	public void testQuery() {
+		ClipperApp.main("-v=2 -rewriter=old query src/test/resources/university.owl src/test/resources/university-q1.sparql"
+				.split("\\ "));
+	}
+
+	public static void main(String[] args) {
+		new ClipperAppTest().testQuery();
 	}
 
 }
