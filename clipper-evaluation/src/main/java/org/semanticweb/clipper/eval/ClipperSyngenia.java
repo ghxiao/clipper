b094af8dc4864610c5d858a253bc0e3eb011e317
xiao <xiao@b55cc70a-2b87-11e0-8e4b-6b21db92dc1d>
2012-08-02 08:20:05 +0000

    add syngenia dependency
    git-svn-id: https://www.kr.tuwien.ac.at/krrepos/xiao/imps/QAHornSHIQ/trunk@2706 b55cc70a-2b87-11e0-8e4b-6b21db92dc1d

---
+++ clipper-evaluation/src/main/java/org/semanticweb/clipper/eval/ClipperSyngenia.java
@@ -0,0 +1,51 @@
+package org.semanticweb.clipper.eval;
+
+import java.io.File;
+
+import org.oxford.comlab.compass.SystemInterface;
+
+public class ClipperSyngenia implements SystemInterface {
+
+	@Override
+	public void initializeSystem() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadTestToSystem(String ontologyFile, File aBox) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadQuery(int queryIndex) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadQuery(String atomSymbol, int symbolType) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long runLoadedQuery() throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clearRepository() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
