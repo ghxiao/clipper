35e46e1cc35fc58b7ee3ece32277df21fa9f8d12
ghxiao <gh.xiao@github.com>
2012-08-16 01:05:55 +0200

    fix a bug with the name of query head predicate

--- clipper-reasoner/src/main/java/org/semanticweb/clipper/hornshiq/queryanswering/CQGraph.java
+++ clipper-reasoner/src/main/java/org/semanticweb/clipper/hornshiq/queryanswering/CQGraph.java
@@ -49,6 +49,8 @@ public class CQGraph extends DirectedSparseMultigraph<Term, CQGraphEdge> {
 	 */
 	private Multimap<Term, Integer> concepts = HashMultimap.create();
 
+	private String headPredicate;
+
 	/**
 	 * never use
 	 */
@@ -63,6 +65,8 @@ public class CQGraph extends DirectedSparseMultigraph<Term, CQGraphEdge> {
 	public CQGraph(CQ cq) {
 		super();
 
+		this.headPredicate = cq.getHead().getPredicate().toString();
+		
 		for (Atom atom : cq.getBody()) {
 			Predicate predicate = atom.getPredicate();
 			List<Term> terms = atom.getTerms();
@@ -101,7 +105,8 @@ public class CQGraph extends DirectedSparseMultigraph<Term, CQGraphEdge> {
 		// g.roles.putAll(this.roles);
 		g.concepts.putAll(this.concepts);
 		g.answerVariables.addAll(this.answerVariables);
-
+		g.headPredicate = this.headPredicate;
+		
 		return g;
 
 	}
@@ -398,7 +403,7 @@ public class CQGraph extends DirectedSparseMultigraph<Term, CQGraphEdge> {
 		}
 
 		List<Term> answerVars = new ArrayList<Term>(answerVariables);
-		Predicate headPredicate = new NonDLPredicate("q");
+		Predicate headPredicate = new NonDLPredicate(this.headPredicate);
 		Atom head = new Atom(headPredicate, answerVars);
 		CQ cq = new CQ();
 		cq.setHead(head);
