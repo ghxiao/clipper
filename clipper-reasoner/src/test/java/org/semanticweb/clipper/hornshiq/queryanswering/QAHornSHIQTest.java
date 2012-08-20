b435424357e0b3921cb184656fbbe39b45faf85b
ghxiao <gh.xiao@github.com>
2012-08-20 20:24:00 +0200

    add a test case corresponding to Example 4 of TR

--- clipper-reasoner/src/test/java/org/semanticweb/clipper/hornshiq/queryanswering/QAHornSHIQTest.java
+++ clipper-reasoner/src/test/java/org/semanticweb/clipper/hornshiq/queryanswering/QAHornSHIQTest.java
@@ -2,6 +2,9 @@ package org.semanticweb.clipper.hornshiq.queryanswering;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +14,17 @@ import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
 import org.junit.Test;
+import org.semanticweb.clipper.cqparser.CQParser;
 import org.semanticweb.clipper.hornshiq.queryanswering.ReductionToDatalogOpt.NamingStrategy;
 import org.semanticweb.clipper.hornshiq.rule.CQ;
 import org.semanticweb.clipper.hornshiq.sparql.SparqlLexer;
 import org.semanticweb.clipper.hornshiq.sparql.SparqlParser;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableSet;
 
 
 public class QAHornSHIQTest {
@@ -56,5 +66,24 @@ public class QAHornSHIQTest {
 		}
 	}
 	
+	@Test
+	public void testTransitivity01() throws OWLOntologyCreationException, FileNotFoundException, IOException{
+		QAHornSHIQ qaHornSHIQ = new QAHornSHIQ();
+		OWLOntology ontology = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(new File("TestCaseOntologies/trans1.owl"));
+		CQParser cqParser = new CQParser(new File("TestCaseOntologies/trans1.cq"), ImmutableSet.of(ontology));
+		CQ cq = cqParser.parse();
+		qaHornSHIQ.addOntology(ontology);
+		qaHornSHIQ.setQuery(cq);
+		qaHornSHIQ.setQueryRewriter("new");
+		ClipperManager.getInstance().setVerboseLevel(1);
+		qaHornSHIQ.setDataLogName("TestCaseOntologies/tmp.dlv");
+		List<List<String>> results = qaHornSHIQ.execQuery();
+		assertEquals(1, results.size());
+		Joiner.on("\n").appendTo(System.out, results);
+	}
+	
+	public static void main(String[] args) throws OWLOntologyCreationException, FileNotFoundException, IOException{
+		new QAHornSHIQTest().testTransitivity01();
+	}
 	
 }
