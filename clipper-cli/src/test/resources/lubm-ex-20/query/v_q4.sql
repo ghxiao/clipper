b437a974f750fcd8c41ad05e719b28dcd359ad3d
Guohui Xiao <gh.xiao@gmail.com>
2012-11-08 03:53:17 +0100

    fix view

--- clipper-cli/src/test/resources/lubm-ex-20/query/v_q4.sql
+++ clipper-cli/src/test/resources/lubm-ex-20/query/v_q4.sql
@@ -4,10 +4,13 @@ SELECT
 v_Subj3Department_1.att1 AS att1 , v_Subj4Department_1.att1 AS att2 
 FROM 
 v_Subj4Department v_Subj4Department_1,
-v_Professor v_Professor_1v_Professor v_Professor_2,
+v_Professor v_Professor_1,
+v_Professor v_Professor_2,
 v_Subj3Department v_Subj3Department_1,
-v_publicationAuthor v_publicationAuthor_1v_publicationAuthor v_publicationAuthor_2,
-v_memberOf v_memberOf_1v_memberOf v_memberOf_2
+v_publicationAuthor v_publicationAuthor_1,
+v_publicationAuthor v_publicationAuthor_2,
+v_memberOf v_memberOf_1,
+v_memberOf v_memberOf_2
 WHERE 
 v_Professor_1.att1 = v_memberOf_1.att1
  AND v_Professor_1.att1 = v_publicationAuthor_1.att2
@@ -26,7 +29,8 @@ v_Subj4Department v_Subj4Department_1,
 v_Professor v_Professor_1,
 v_Subj3Department v_Subj3Department_1,
 v_Faculty v_Faculty_1,
-v_memberOf v_memberOf_1v_memberOf v_memberOf_2
+v_memberOf v_memberOf_1,
+v_memberOf v_memberOf_2
 WHERE 
 v_memberOf_1.att1 = v_Faculty_1.att1
  AND v_memberOf_1.att1 = v_Professor_1.att1
--- clipper-cli/src/test/resources/lubm-ex-20/query/v_q6.sql
+++ clipper-cli/src/test/resources/lubm-ex-20/query/v_q6.sql
@@ -5,8 +5,10 @@ v_University_1.att1 AS att1 , v_memberOf_2.att2 AS att2
 FROM 
 v_advisor v_advisor_1,
 v_Student v_Student_1,
-v_memberOf v_memberOf_1v_memberOf v_memberOf_2,
-v_University v_University_1v_University v_University_2
+v_memberOf v_memberOf_1,
+v_memberOf v_memberOf_2,
+v_University v_University_1,
+v_University v_University_2
 WHERE 
 v_memberOf_1.att1 = v_advisor_1.att2
  AND v_Student_1.att1 = v_memberOf_1.att1
@@ -22,8 +24,10 @@ FROM
 v_Professor v_Professor_1,
 v_advisor v_advisor_1,
 v_Student v_Student_1,
-v_memberOf v_memberOf_1v_memberOf v_memberOf_2,
-v_University v_University_1v_University v_University_2
+v_memberOf v_memberOf_1,
+v_memberOf v_memberOf_2,
+v_University v_University_1,
+v_University v_University_2
 WHERE 
 v_memberOf_1.att1 = v_Professor_1.att1
  AND v_memberOf_1.att1 = v_advisor_1.att2
