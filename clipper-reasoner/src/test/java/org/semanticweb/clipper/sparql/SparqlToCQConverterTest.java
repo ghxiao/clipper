e83b7d4225614b8443fa8a3d402f1a0bcf7c84b3
Guohui Xiao <gh.xiao@gmail.com>
2012-12-15 19:07:10 +0100

    ignore intellij generated files

---
+++ clipper-reasoner/src/test/java/org/semanticweb/clipper/sparql/SparqlToCQConverterTest.java
@@ -0,0 +1,14 @@
+package org.semanticweb.clipper.sparql;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SparqlToCQConverterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
