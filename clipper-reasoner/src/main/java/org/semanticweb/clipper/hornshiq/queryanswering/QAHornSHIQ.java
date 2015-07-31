92620c0fcda14d69e2997b24fd323b4c8f0bf57f
Guohui Xiao <gh.xiao@gmail.com>
2015-07-31 20:26:04 +0200

    handle imports properly

--- clipper-reasoner/src/main/java/org/semanticweb/clipper/hornshiq/queryanswering/QAHornSHIQ.java
+++ clipper-reasoner/src/main/java/org/semanticweb/clipper/hornshiq/queryanswering/QAHornSHIQ.java
@@ -300,6 +300,7 @@ public class QAHornSHIQ implements QueryAnsweringSystem {
 	 * @return
 	 */
 	private OWLOntology normalize(OWLOntology ontology) {
+        
 		HornSHIQProfile profile = new HornSHIQProfile();
 
 		OWLProfileReport report = profile.checkOntology(ontology);
@@ -703,8 +704,10 @@ public class QAHornSHIQ implements QueryAnsweringSystem {
 		}
 
 		for (OWLOntology ontology : ontologies) {
-			ontology = normalize(ontology);
-			manager.addAxioms(combinedOntology, ontology.getAxioms());
+            for (OWLOntology ontologyInImportsClosure : ontology.getImportsClosure()) {
+                OWLOntology normalizedOntology = normalize(ontologyInImportsClosure);
+                manager.addAxioms(combinedOntology, normalizedOntology.getAxioms());
+            }
 		}
 
 		ClipperHornSHIQOntologyConverter converter = new ClipperHornSHIQOntologyConverter();
