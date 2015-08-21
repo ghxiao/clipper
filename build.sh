73f26c88d33666dbe24f43c38f67ec3268b965ad
Guohui Xiao <gh.xiao@gmail.com>
2015-08-21 23:23:34 +0200

    mvn_build.sh -> build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#mvn clean
-mvn install -Dmaven.test.skip=true 
+mvn clean install -Dmaven.test.skip=true
 cd clipper-distribution/
 mvn assembly:assembly 
 unzip -o target/clipper-bin.zip -d target/
