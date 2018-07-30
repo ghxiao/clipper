de5395fb7bcb76ad28b59760e705e9340c8a0c21
labiNOT <labinotbajraktari@gmail.com>
2018-07-30 15:02:55 +0200

    updated tbox reasoner

---
+++ clipper-reasoner/src/test/java/org/semanticweb/clipper/hornshiq/aboxprofile/ABoxProfileLoaderTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by bato on 7/30/18.
+ */
+public class ABoxProfileLoaderTest {
+
+}
---
+++ clipper-reasoner/src/test/java/org/semanticweb/clipper/hornshiq/queryanswering/TBoxReasonerWithActivatorsTest.java
@@ -0,0 +1,7 @@
+package org.semanticweb.clipper.hornshiq.queryanswering;
+
+/**
+ * Created by bato on 7/29/18.
+ */
+public class TBoxReasonerWithActivatorsTest {
+}
